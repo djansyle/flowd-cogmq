309df5e6ca01ab2f29ee1014f3672783de8b7aab
djansyle <djansyledjans@gmail.com>
2017-07-17 06:47:43 +0800

    bump(version): version to v1.0.9

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Server = require('./server');
 const Application = require('./application');
+const cogmq = require('cogmq').default;
 
 module.exports = {
-  CogServer: Server, CogApplication: Application
+  CogServer: Server, CogApplication: Application, CogError: cogmq.Error
 };
